ba1382dbeff0f0f1044e17fa4994a4275f3a86b8
pratyushshivam <pratyushshivam7@gmail.com>
2021-04-02 01:03:57 +0530

    post

---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.demo.security.config;
+
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // to send post requests without being rejected
+                .authorizeRequests()
+                    .antMatchers("/api/v*/registration/**") // permit all these type of requests
+                    .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+        // any request that goes like this / through these endpoints, we want to allow it.
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+
+    }
+}
+
+// configuration needed for security
+//securing endpoints
